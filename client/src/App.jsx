4650cc5a96eec3ef9cd9cff759b2c4bd601e7f06
Manuel Alingog <alingman01@gmail.com>
2024-02-28 08:59:32 -0800

    re do repo up to client and server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+
 import './App.css';
 import sqlLogo from './assets/sql-server.png';
 
 function App() {
+
+  const [queryDescription, setQueryDescription] = useState('');
+  const [sqlQuery, setSqlQuery] = useState('');
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(queryDescription);
+  }
+
   return (
     <main className="main">
         <img src={sqlLogo} alt="sql Logo" className='icon' />
        <h3>Chat My Database</h3>
-       <form>
+       <form onSubmit={onSubmit}>
         <input 
           type="text"
           name='query-description' 
           placeholder='What question do you want answered?'
+          onChange={(e) => setQueryDescription(e.target.value)}
         />
         <input 
           type="submit" 
